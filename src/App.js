d9d3743d924d6a0ac6d1b94b7a859694666c3e08
Fabio <fabioclear@gmail.com>
2019-08-08 16:07:06 +0200

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>Welcome to our Random Shop!</h1>
+      <p>Discover all we got to sell by clicking the button below</p>
+      <button>
+        <Link to="/shop/">Go to shop</Link>
+      </button>
+    </Router>
   );
 }
 
